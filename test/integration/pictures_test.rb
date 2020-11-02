a9e8b76256086b7dc56ecd625d57fdeab6c32439
Riccardo Decandia <riccardo.decandia@gmail.com>
2020-11-02 19:12:11 +0100

    add a dot

--- test/integration/pictures_test.rb
+++ test/integration/pictures_test.rb
@@ -32,18 +32,22 @@ class PicturesTest < ActionDispatch::IntegrationTest
     assert_match @user.username, response.body
   end
   
-  test "create new valid picture" do
-     get new_picture_path 
-    assert_template 'pictures/new'
-    name_of_picture = "chicken saute"  description_of_picture = "add chicken, add vegetables, cook for 20 minutes, serve delicious meal"
-      assert_difference 'Picture.count', 1 do
-        post pictures_path, params: { picture: { name: name_of_picture, 
-                                                       description: description_of_picture}}
-      end
-      follow_redirect!
-      assert_match name_of_picture.capitalize, response.body
-      assert_match description_of_picture, response.body
-   end
+  test "create new valid recipe" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    name_of_recipe = "chicken saute"
+    description_of_recipe = "add chicken, add vegetables, 
+  
+                       cook for 20 minutes, serve delicious meal"
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: { recipe: { name: name_of_recipe, 
+  
+                                                     description: description_of_recipe}}
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe, response.body
+  end
   
     
   test "reject invalid picture submissions" do
